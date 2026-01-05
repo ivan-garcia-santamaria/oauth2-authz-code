9da98754860156a902412d6101cad508443a22a8
davidherlop11 <david.hernandezlopez@masmovil.com>
2026-01-05 13:25:02 +0100

    add-hybrid-flow

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 const express = require('express'),
     bunyan = require('bunyan'),
     bodyParser = require('body-parser'),
-    fetch = require("node-fetch");
     axios = require('axios');
+
+const fetch = require('node-fetch').default;
+
 
 //Load values from .env file
 require('dotenv').config();
@@ -66,6 +68,13 @@ app.get('/authorize', (req, res) => {
     client_secret = req.query.client_secret;
     host = req.query.host;
     res.render('index');
+});
+
+app.get('/authorize-hybrid', (req, res) => {
+    client_id = req.query.client_id;
+    client_secret = req.query.client_secret;
+    host = req.query.host;
+    res.render('index-hybrid');
 });
 
 app.get('/token-page', (req, res) => {
@@ -252,7 +261,7 @@ app.get('/check-ciba', async (req, res) => {
     }
 });
 
-//Set 1: Ask the authorization code
+//Set 1: Ask the authorization code (Standard flow)
 app.get('/get/the/code', async (req, res) => {
 
 
@@ -270,8 +279,47 @@ app.get('/get/the/code', async (req, res) => {
         Redirect_Uri = process.env.REDIRECT_OVERRIDE;
     }
 
-    let url = `${Authorization_Endpoint}?response_type=${Response_Type}&client_id=${client_id}&redirect_uri=${Redirect_Uri}&state=${State}&groups_hint=${process.env.GROUPS_HINT}&login_hint=${process.env.LOGIN_HINT}&access_type=${process.env.ACCESS_TYPE}`;
-    // let url = `${Authorization_Endpoint}?response_type=${Response_Type}&client_id=${client_id}&redirect_uri=${Redirect_Uri}&scope=${Scope}&state=${State}&groups_hint=${process.env.GROUPS_HINT}&login_hint=${process.env.LOGIN_HINT}&access_type=${process.env.ACCESS_TYPE}`;
+    let url = `${Authorization_Endpoint}?response_type=${Response_Type}&client_id=${client_id}&redirect_uri=${Redirect_Uri}&scope=${Scope}&state=${State}&groups_hint=${process.env.GROUPS_HINT}&login_hint=${process.env.LOGIN_HINT}&access_type=${process.env.ACCESS_TYPE}`;
+
+    log.info(url);
+
+    if (Scope == "number-verification-verify-read") {
+        const headers = {
+            'User-Identity-Forward-msisdn': '34636606875',
+        };
+
+        try {
+            const respuesta = await axios.get(url, { headers });
+
+            res.status(respuesta.status).send(respuesta.data);
+        } catch (error) {
+            console.error('Error en la solicitud de redirección:', error.message);
+            res.status(500).send('Error en la redirección');
+        }
+    } else {
+
+        res.redirect(url);
+    }
+});
+
+//Hybrid Flow: Ask the authorization code with id_token and token
+app.get('/get/the/code/hybrid', async (req, res) => {
+
+    const Authorization_Endpoint = `${host}/oauth/authorize`;
+    const Response_Type = 'code id_token token';
+    const Redirect_Uri_Default = `http://${process.env.LOCAL_DOMAIN}:8000/give/me/the/code`;
+    const Scope = process.env.SCOPE;
+    const State = `${uuid.v1()}`;
+
+    log.info("redirect: " + process.env.REDIRECT_OVERRIDE)
+
+    if (process.env.REDIRECT_OVERRIDE === undefined) {
+        Redirect_Uri = Redirect_Uri_Default;
+    }else {
+        Redirect_Uri = process.env.REDIRECT_OVERRIDE;
+    }
+
+    let url = `${Authorization_Endpoint}?response_type=${Response_Type}&client_id=${client_id}&redirect_uri=${Redirect_Uri}&state=${State}&groups_hint=${process.env.GROUPS_HINT}&login_hint=${process.env.LOGIN_HINT}&access_type=${process.env.ACCESS_TYPE}&scope=openid&nonce=n-0S6_WzA2Mj`;
 
     log.info(url);
 
